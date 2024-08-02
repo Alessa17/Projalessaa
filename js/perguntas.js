8e52b6a159aeb8778828ada5dfcd378610a3fbb4
Alessa17 <137888294+Alessa17@users.noreply.github.com>
2024-08-02 15:43:13 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,9 +1,9 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        // enunciado: "Ao pensar em campo literário, o que você defende?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Defendo que os melhores livros são os de períodos históricos, pois apresentam grande valores para a sociedade atual!!",
                 afirmacao: [
                     "No início ficou com medo do que essa tecnologia pode fazer.",
                     "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
@@ -11,7 +11,7 @@ export const perguntas = [
                 proxima: 1,
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Defendo que os melhores livros são os de ficção, pois fogem da realidade!",
                 afirmacao: [
                     "Quis saber como usar IA no seu dia a dia.",
                     "Pensou que IA pode ajudar em tarefas da sua vida."
